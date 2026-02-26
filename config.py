f3802709dece15c4379c0fa7ccd7a93e9ade79e8
DVITTALBOTZ <133628140+DVITTALBOTZ@users.noreply.github.com>
2026-02-26 22:54:11 +0530

    Update default DATABASE_CHANNEL in config.py

--- config.py
+++ config.py
@@ -25,7 +25,7 @@ HELP_TXT =  os.environ.get("HELP_MESSAGE", "⁉️ Hᴇʟʟᴏ {mention} ~\n\n <
 FSUB_PIC = os.environ.get("FSUB_PIC", "https://ibb.co/jPPStQw9")
 FSUB_LINK_EXPIRY = 300
 LOG_FILE_NAME = "DVITTALBOTZ.txt"
-DATABASE_CHANNEL = int(os.environ.get("DATABASE_CHANNEL", ""))
+DATABASE_CHANNEL = int(os.environ.get("DATABASE_CHANNEL", "-1002395841344"))
 
 logging.basicConfig(
     level=logging.INFO,
